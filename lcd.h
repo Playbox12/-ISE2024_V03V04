092dc02be2ab0d883ee38544fc2036f21d16eb3e
Proleta-Dimitrova <169035296+Proleta-Dimitrova@users.noreply.github.com>
2024-05-06 13:05:39 +0200

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,38 @@
+#ifndef __LCD_H
+#define __LCD_H
+
+#include "Driver_SPI.h"
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+
+# define PIN_RESET GPIO_PIN_6 /*PA6*/
+# define PIN_A0  GPIO_PIN_13 /*PF13*/
+# define PIN_CS  GPIO_PIN_14 /*PD14*/
+
+extern char mensajeL1[100];
+extern char mensajeL2[100];
+
+extern ARM_DRIVER_SPI Driver_SPI1;
+
+extern unsigned char buffer[512];
+
+void delay(uint32_t n_microsegundos);
+void LCD_reset(void);
+void LCD_init(void);
+void LCD_wr_data(unsigned char data);
+void LCD_wr_cmd(unsigned char cmd);
+void LCD_update_L1(void);
+void LCD_update_L2(void);
+void symbolToLocalBuffer_L1(uint8_t symbol);
+void symbolToLocalBuffer_L2(uint8_t symbol);
+void symbolToLocalBuffer(uint8_t line, uint8_t symbol);
+void escribirLinea(uint8_t line, char* mensaje);
+void escribirLinea_L1(char* mensaje);
+void escribirLinea_L2(char* mensaje);
+void limpiarLCD(void);
+void limpiarLCD_L1(void);
+void limpiarLCD_L2(void);
+
+
+#endif /* _LCD_H */
