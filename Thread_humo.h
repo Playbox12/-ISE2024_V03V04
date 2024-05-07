23c4c08c670f5dded1353bebb29b3e7cd5a6d654
Proleta-Dimitrova <169035296+Proleta-Dimitrova@users.noreply.github.com>
2024-05-07 15:47:05 +0200

    Update Thread_humo.h

--- Thread_humo.h
+++ Thread_humo.h
@@ -1,20 +1,11 @@
 #include "cmsis_os2.h"
-#include "adc.h"
 #include <math.h>
 #include <stdio.h>
 #ifndef __HUMO_H
-
-
-
-	
 
 //INICIAR THREAD HUMO
 int Init_Thread_humo (void);
 void HUMO_init(void);
 void LED_Init(void);
-float readMQ(void);
-float getMQResistence(int raw_adc);
-float getConcentracion(float rs_ro_ratio);
-float calculo_concentracion(void);
-float SensorCalibration(void);
+
 #endif
